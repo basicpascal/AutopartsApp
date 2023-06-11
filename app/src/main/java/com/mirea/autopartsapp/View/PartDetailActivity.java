169d8e7cf8b4db4b97d5f65e82722e5d4be1b15e
Danila Kalugin <danilakalugin@yandex.ru>
2023-06-12 01:16:16 +0300

    Fixed the title of toolbar in activity of detailed page of part, passing the name of the element to it

--- app/src/main/java/com/mirea/autopartsapp/View/PartDetailActivity.java
+++ app/src/main/java/com/mirea/autopartsapp/View/PartDetailActivity.java
@@ -6,12 +6,18 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import com.bumptech.glide.Glide;
 import com.mirea.autopartsapp.Model.Part;
 import com.mirea.autopartsapp.R;
+
 import java.util.Objects;
 
 public class PartDetailActivity extends AppCompatActivity {
+
+    public static final String EXTRA_PART = "part";
+
+    private ImageView partImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,24 +28,25 @@ public class PartDetailActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
 
-        ImageView partImageView = findViewById(R.id.partImageView);
+        partImageView = findViewById(R.id.partImageView);
         TextView partDescriptionTextView = findViewById(R.id.partDescriptionTextView);
 
-        Part part = getIntent().getParcelableExtra("part");
-        String imageUrl = part.getImageUrl();
+        if (getIntent().hasExtra(EXTRA_PART)) {
+            Part part = getIntent().getParcelableExtra(EXTRA_PART);
+            if (part != null) {
+                setTitle(part.getName());
+                loadImage(part.getImageUrl());
+                partDescriptionTextView.setText(part.getDescription());
+            }
+        }
+    }
 
-        // Загрузка и отображение изображения
-        if (imageUrl != null && !imageUrl.isEmpty()) {
-            Glide.with(this)
-                    .load(imageUrl)
-                    .placeholder(R.drawable.placeholder_image)
-                    .error(R.drawable.error_image)
-                    .into(partImageView);
-        }
-        else {
-            partImageView.setImageResource(R.drawable.error_image);
-        }
-            partDescriptionTextView.setText(part.getDescription());
+    private void loadImage(String imageUrl) {
+        Glide.with(this)
+                .load(imageUrl)
+                .placeholder(R.drawable.placeholder_image)
+                .error(R.drawable.error_image)
+                .into(partImageView);
     }
 
     @Override
